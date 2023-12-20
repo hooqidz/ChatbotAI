44e32fe8c8303cc9dc3beeda871cc914d13bd5a0
Erin Fajrin Nugraha <hooqidz@gmail.com>
2023-12-20 14:59:17 +0700

    Install torch

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,6 +1,8 @@
 from nltk.stem.porter import PorterStemmer
 import nltk
-nltk.download('punkt')
+import numpy as np
+
+# nltk.download('punkt')
 stemmer = PorterStemmer()
 
 
@@ -13,14 +15,23 @@ def stem(word):
 
 
 def bag_of_words(tokenized_sentence, all_words):
-    pass
+    """
+    sentence = ["hello","how","are","you"]
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bog =   [ 0,     1,      0,    1,     0,      0,      0]
+    """
+
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
+
+    return bag
 
 
-a = "How long does delivery take?"
-print(a)
-a = tokenize(a)
-print(a)
-
-words = ["Organize", "organizes", "organizing"]
-stemmed_words = [stem(w) for w in words]
-print(stemmed_words)
+sentence = ["hello", "how", "are", "you"]
+words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+bog = bag_of_words(sentence, words)
+print(bog)
--- train.py
+++ train.py
@@ -1,6 +1,9 @@
 import json
 from nltk_utils import tokenize, stem, bag_of_words
 
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, Dataloader
 
 with open('intents.json', 'r') as f:
     intents = json.load(f)
@@ -17,4 +20,21 @@ for intent in intents["intents"]:
         xy.append((w, tag))
 
 ignore_words = ['?', '!', '.', ',']
-print(all_words)
+all_words = [stem(w) for w in all_words if w not in ignore_words]
+all_words = sorted(set(all_words))
+tags = sorted(set(tags))
+print(tags)
+
+
+x_train = []
+y_train = []
+for (pattern_sentence, tag) in xy:
+
+    bag = bag_of_words(pattern_sentence)
+    x_train.append(bag)
+
+    label = tags.index(tag)
+    y_train.append(label)
+
+x_train = np.array(x_train)
+y_train = np.array(x_train)
